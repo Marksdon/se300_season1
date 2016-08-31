76cd1548ff70f39b44b9a211238738e789d70ad4
Marksdon <412176694@qq.com>
2016-09-01 01:24:29 +0800

    commint haha

--- src/com/xzc/se300season1/test/HelloSeasonOne.java
+++ src/com/xzc/se300season1/test/HelloSeasonOne.java
@@ -3,5 +3,6 @@ package com.xzc.se300season1.test;
 public class HelloSeasonOne {
 	public static void main(String[] args) {
 		System.out.println("hello seansonOne");
+		System.out.println("haha,hey");
 	}
 }
