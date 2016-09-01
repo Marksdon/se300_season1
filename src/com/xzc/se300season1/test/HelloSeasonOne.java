0c37ad116ce9bd954324c48807d7997c1fde934c
Marksdon <412176694@qq.com>
2016-09-01 10:17:41 +0800

    a JDBCUtil define

--- src/com/xzc/se300season1/test/HelloSeasonOne.java
+++ src/com/xzc/se300season1/test/HelloSeasonOne.java
@@ -3,6 +3,5 @@ package com.xzc.se300season1.test;
 public class HelloSeasonOne {
 	public static void main(String[] args) {
 		System.out.println("hello seansonOne");
-		System.out.println("haha,hey");
 	}
 }
---
+++ src/com/xzc/se300season1/util/JDBCUtil.java
@@ -0,0 +1,16 @@
+package com.xzc.se300season1.util;
+
+/**
+ * this class provide a serial util for the operation with database
+ * no matter what databases, MySQL or Oracle
+ * @author randal
+ *
+ */
+public class JDBCUtil {
+	
+	//getMySQLConnection()
+	//closeMySQLConnection()
+	
+	//getOracleConnection()
+	//closeOracleConnection()
+}
